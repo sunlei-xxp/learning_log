07c2192d6580b96ae1bd20d70d8db888fbd0985d
sunlei-xxp <sun-lei@163.com>
2021-03-24 23:54:23 +0800

    增加注销链接, 完成19.2.3

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ from . import views
 
 urlpatterns = [
 	#登陆页面
-	url(r'^login/$',LoginView.as_view(template_name='users/login.html'), name='login'),
+	url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+	#注销
+	url(r'^logout/$', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 from django.shortcuts import render
 
 # Create your views here.
+
+def logout_view(request):
+	"""注销用户"""
+	logout(request)
+	return HttpResponseRedirect(reverse('learning_logs:index'))
